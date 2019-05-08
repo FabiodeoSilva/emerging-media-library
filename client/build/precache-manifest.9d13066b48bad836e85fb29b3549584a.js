a1d8434a825b53dae0e6360c820be2bd0b415290
FabiodeoSilva <FabiodeoSilva@gmail.com>
2019-05-08 04:24:41 -0400

    test

---
+++ client/build/precache-manifest.9d13066b48bad836e85fb29b3549584a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1872a9ccd8948f4527d16367e1d2cc55",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cbf64b8c97592dc033d7",
+    "url": "/static/css/main.d8116c63.chunk.css"
+  },
+  {
+    "revision": "1e40b66b7c002d1eb770",
+    "url": "/static/js/2.e1f72eab.chunk.js"
+  },
+  {
+    "revision": "cbf64b8c97592dc033d7",
+    "url": "/static/js/main.05248433.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4b31330b20e3cff3e49d540e4e7175e2",
+    "url": "/static/media/showcase.4b31330b.jpg"
+  }
+]);
